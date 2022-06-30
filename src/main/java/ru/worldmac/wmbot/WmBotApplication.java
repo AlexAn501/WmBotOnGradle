0de7160965bce74d99db6741554f9223751f4668
aaantonov <aaantonov@phoenixit.ru>
2022-06-30 23:17:18 +0300

    git init

---
+++ src/main/java/ru/worldmac/wmbot/WmBotApplication.java
@@ -0,0 +1,13 @@
+package ru.worldmac.wmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/worldmac/wmbot/WmBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.worldmac.wmbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmBotApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
